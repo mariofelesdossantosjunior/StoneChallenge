2b7907b999b1f773b0d06a6324fe1776b238c592
Mario Feles dos Santos Junior <mario_feles@live.com>
2024-12-03 22:46:28 -0300

    feat: remove dependencies

--- composeApp/src/commonMain/kotlin/com/mario/stonechallenge/presentation/products/ProductsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/mario/stonechallenge/presentation/products/ProductsScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.mario.stonechallenge.presentation.components.ProductsList
 import com.mario.stonechallenge.presentation.components.ProgressIndicator
 import com.mario.stonechallenge.presentation.components.TopBar
